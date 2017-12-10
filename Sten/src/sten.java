cca6ac48b10a4d1bfc796afe704dea34dbd35e22
Administrator <Administrator@Admin-PC.mshome.net>
2017-12-10 22:36:00 +0100

    tilføjet 2 forskellige metoder

--- Sten/src/sten.java
+++ Sten/src/sten.java
@@ -4,9 +4,56 @@ public class sten {
 
 	public static void main(String[] args)
 	{
-
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("velkommen til, vælg en af følgende 3 muligheder");
+		System.out.println("1 find farten ved et fald på jorden");
+		System.out.println("2 find farten ved et fald på månen\n");
+	
+		int i = sc.nextInt();
+		
+		if (i==1)
+			fartVedFald(sc);
+		if (i==2) 
+		farVedFaldMåne(sc);
+		else
 
+		sc.close();
+		
+	}
+
+	private static void farVedFaldMåne(Scanner sc) {
+		System.out.print("Angiv hÃ¸jde: ");
+		double h=0.0;
+
+		while(true) {
+			if(sc.hasNextDouble()) {
+				h = sc.nextDouble();
+				if(h <= 0)
+					System.out.print("ugyldigt input \nIndtast et tal. prÃ¸v igen: ");
+				else break;		
+			}
+			else {
+				System.out.print("ugyldigt input \nIndtast et tal. prÃ¸v igen: ");
+				sc.next();				
+			}	
+		}
+		double v;
+		double t;
+
+		v = Math.sqrt(2 * 1.6 * h);
+		t = v/1.6;
+
+		System.out.println("HÃ¸jden er  \t" + h +" m");	
+		System.out.print("Farten er \t");
+		System.out.println(Math.round(v*100.0)/100.0 + " m/s");
+		System.out.println("\t\t" + Math.round(v*3.6*100.0)/100.0 + " km/t");
+		System.out.print("Tiden i luften er ");
+		System.out.println(Math.round(t*100.0)/100.0 + " s");
+		
+	}
+
+	private static void fartVedFald(Scanner sc) {
 		System.out.print("Angiv hÃ¸jde: ");
 		//double h= input.nextDouble(); 
 		double h=0.0;
@@ -35,10 +82,11 @@ public class sten {
 		System.out.println("\t\t" + Math.round(v*3.6*100.0)/100.0 + " km/t");
 		System.out.print("Tiden i luften er ");
 		System.out.println(Math.round(t*100.0)/100.0 + " s");
+	}
+}
 
 
-		sc.close();
-		//-----------------------------------------------------------------------//
+//-----------------------------------------------------------------------//
 		//Programmet udregner hastigheden af en sten efter at den er accelereret 
 		//fra en given hï¿½jde. 
 		//programmet er skrevet af Alexander Kjeldsen og Yoss Mellin.
@@ -47,5 +95,3 @@ public class sten {
 		//der er nu lavet et loop, sï¿½ der ikke kommer en "error", nï¿½r man skriver
 		//et bogstav eller et negativt tal.
 		//----------------------------------------------------------------------//
-	}
-}
